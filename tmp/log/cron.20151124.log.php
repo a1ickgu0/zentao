32d0380662318a980bd3158c053e0c2e4fe3d07b
a1ickgu0 <guow.pub@gmail.com>
2015-11-30 08:12:57 +0800

    init version

---
+++ tmp/log/cron.20151124.log.php
@@ -0,0 +1,222 @@
+<?php
+ die();
+15:15:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:20:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:25:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:30:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:35:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:40:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:45:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:50:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+15:55:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:00:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:05:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:10:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:15:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:20:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:25:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:30:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:35:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:40:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:45:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:50:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+16:55:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:00:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:05:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:10:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:15:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:20:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:25:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:30:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:35:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:40:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:45:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:50:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+17:55:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:00:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:05:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:10:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:15:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:20:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:25:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:30:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:35:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:40:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+18:45:24 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
+19:10:12 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : OK
+
---
+++ tmp/log/php.20151124.log.php
@@ -0,0 +1,34 @@
+<?php
+ die();
+?>
+
+15:07:32 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php
+
+15:07:34 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php
+
+15:07:35 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php
+
+15:08:33 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php?m=install&f=license
+
+15:08:34 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php?m=install&f=step1
+
+15:08:37 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php?m=install&f=step2
+
+15:09:31 Undefined property: config::$debug in /var/zentao/zentaopms/module/common/view/header.lite.html.php on line 20 when visiting /zentao/install.php?m=install&f=step3
+
+15:11:23 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
+
+15:38:57 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
+
+18:50:24 ERROR: SQLSTATE[HY000]: General error: 2006 MySQL server has gone awayThe sql is: SELECT * FROM `zt_config` wHeRe owner  = 'system' AND module  = 'common' AND section  = 'global' AND `key`  = 'cron' in /var/zentao/zentaopms/lib/dao/dao.class.php on line 1058, last called by /var/zentao/zentaopms/lib/dao/dao.class.php on line 511 through function sqlError.
+ in /var/zentao/zentaopms/framework/router.class.php on line 1568 when visiting /zentao/index.php?m=cron&f=ajaxExec&t=html
+
+19:15:00 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
+
+19:15:02 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
+
+19:17:14 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
+
+20:04:08 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
+
+22:02:08 Undefined offset: 1 in /var/zentao/zentaopms/module/user/control.php on line 691 when visiting /zentao/index.php?m=user&f=login&referer=L3plbnRhby8=
